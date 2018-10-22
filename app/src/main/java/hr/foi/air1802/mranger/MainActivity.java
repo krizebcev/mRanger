d0ee4a1082624669b1c19ab99bb58742c13980eb
Fabijan <37190757+fkraljic@users.noreply.github.com>
2018-10-22 18:51:08 +0200

    Kreiran prazan projekt
    Kreiran je prazni projekt za aplikaciju mRanger.

---
+++ app/src/main/java/hr/foi/air1802/mranger/MainActivity.java
@@ -0,0 +1,13 @@
+package hr.foi.air1802.mranger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
