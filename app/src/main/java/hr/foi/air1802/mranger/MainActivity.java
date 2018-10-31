4bbb36c0249ea3d763dfbeb30bafafb3da81bd35
Jakov Kristović <37014410+jkristovi@users.noreply.github.com>
2018-10-31 19:20:13 +0100

    Dodavanje novog zaslona u projekt
    Dodavanje novog zaslona u projekt i omogućavanje navigacije sa početnog zaslona na zaslon za upravljanje

--- app/src/main/java/hr/foi/air1802/mranger/MainActivity.java
+++ app/src/main/java/hr/foi/air1802/mranger/MainActivity.java
@@ -1,13 +1,29 @@
 package hr.foi.air1802.mranger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button gumbPoveziBluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gumbPoveziBluetooth = (Button) findViewById(R.id.gumbPoveziBluetooth);
+
+        gumbPoveziBluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent kontroleActivity = new Intent(getApplicationContext(), KontroleActivity.class);
+                startActivity(kontroleActivity);
+            }
+        }); //kraj gumbPoveziBluetooth
+
+
     }
 }
