134b90bc2c504a3350df8f9963a5aab37809af77
Jakov Kristović <37014410+jkristovi@users.noreply.github.com>
2018-11-07 22:39:05 +0100

    Ostvareno uključivanje Bluetootha
    1) Implementirana mogućnost on/off Bluetootha unutar aplikacije
    2) Povratna informacija ukoliko uređaj ne podržava Bluetooth povezivanje
    
    Co-Authored-By: Fabijan <fkraljic@users.noreply.github.com>
    Co-Authored-By: Karlo Pavleka <kpavleka@users.noreply.github.com>
    Co-Authored-By: Kristijan Perković <kristijan-perkovic96@users.noreply.github.com>
    Co-Authored-By: Kristijan Žebčević <krizebcev@users.noreply.github.com>

--- app/src/main/java/hr/foi/air1802/mranger/MainActivity.java
+++ app/src/main/java/hr/foi/air1802/mranger/MainActivity.java
@@ -1,16 +1,27 @@
 package hr.foi.air1802.mranger;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     //definiranje varijabli
     private Button gumbBTOnOff;
     private Button gumbDiscover;
     private ListView listaDiscoveredDevices;
+
+    private BluetoothAdapter myBluetoothAdapter;
+    private ArrayList<BluetoothDevice> myBluetoothDevices;
 
 
     @Override
@@ -22,6 +33,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gumbBTOnOff = findViewById(R.id.gumbBTOnOff);
+        gumbDiscover = findViewById(R.id.gumbDiscover);
+        listaDiscoveredDevices = findViewById(R.id.listaDiscDevices);
+        listaDiscoveredDevices.setOnItemClickListener(MainActivity.this);
+
+        myBluetoothDevices = new ArrayList<>();
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //event za gumb ON/OFF
+        gumbBTOnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (myBluetoothAdapter == null) { //ne podržava Bluetooth
+                    Toast.makeText(getApplicationContext(),"This device does not support Bluetooth connection.",Toast.LENGTH_LONG).show();
+                }
+                if (!myBluetoothAdapter.isEnabled()) { //Bluetooth nije uključen
+                    Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE); //uključi
+                    startActivity(enableBTIntent);
+                }
+                if (myBluetoothAdapter.isEnabled()) { //Bluetooth uključen
+                    myBluetoothAdapter.disable();
+                    Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE); //isključi
+                    startActivity(enableBTIntent);
+                }
+            }
+        });
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
