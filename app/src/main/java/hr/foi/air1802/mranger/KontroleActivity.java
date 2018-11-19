94cb1a1063dcb70215bcdb18484205abdc5aad56
krizebcev <krizebcev@foi.hr>
2018-11-19 23:00:00 +0100

    Potpuno omogućeno mjerenje temperature
    Potpuno omogućeno mjerenje temperature - pronađena kombinacija parametara za zahtjev prema robotu kojim vraća ispravnu temperaturu.

--- app/src/main/java/hr/foi/air1802/mranger/KontroleActivity.java
+++ app/src/main/java/hr/foi/air1802/mranger/KontroleActivity.java
@@ -110,11 +110,11 @@ public class KontroleActivity extends AppCompatActivity {
 
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
 
-                    Kretanje(-LijeviMotor,DesniMotor);
+                    Kretanje(-LijeviMotor, DesniMotor);
 
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
 
-                    Kretanje(0,0);
+                    Kretanje(0, 0);
                 }
                 return true;
             }
@@ -128,11 +128,11 @@ public class KontroleActivity extends AppCompatActivity {
 
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
 
-                    Kretanje(-LijeviMotor,-DesniMotor);
+                    Kretanje(-LijeviMotor, -DesniMotor);
 
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
 
-                    Kretanje(0,0);
+                    Kretanje(0, 0);
                 }
                 return true;
             }
@@ -144,11 +144,11 @@ public class KontroleActivity extends AppCompatActivity {
 
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
 
-                    Kretanje(LijeviMotor,DesniMotor);
+                    Kretanje(LijeviMotor, DesniMotor);
 
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
 
-                    Kretanje(0,0);
+                    Kretanje(0, 0);
                 }
                 return true;
             }
@@ -159,11 +159,11 @@ public class KontroleActivity extends AppCompatActivity {
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN) {
 
-                    Kretanje(LijeviMotor,-DesniMotor);
+                    Kretanje(LijeviMotor, -DesniMotor);
 
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
 
-                    Kretanje(0,0);
+                    Kretanje(0, 0);
                 }
                 return true;
             }
@@ -183,7 +183,7 @@ public class KontroleActivity extends AppCompatActivity {
         gumbNormalno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PromjenaBrzine(180,180);
+                PromjenaBrzine(180, 180);
 
                 gumbSporo.setBackgroundResource(android.R.drawable.btn_default);
                 gumbNormalno.setBackgroundColor(Color.parseColor("#fed63c"));
@@ -194,7 +194,7 @@ public class KontroleActivity extends AppCompatActivity {
         gumbBrzo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PromjenaBrzine(240,240);
+                PromjenaBrzine(240, 240);
 
                 gumbSporo.setBackgroundResource(android.R.drawable.btn_default);
                 gumbNormalno.setBackgroundResource(android.R.drawable.btn_default);
@@ -207,7 +207,7 @@ public class KontroleActivity extends AppCompatActivity {
         gumbTemperatura.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               gumbTemperatura.setText(DohvatiTemperaturu());
+                gumbTemperatura.setText(DohvatiTemperaturu());
             }
         });
 
@@ -215,14 +215,13 @@ public class KontroleActivity extends AppCompatActivity {
 
 
     //metoda za kretanje
-    private void Kretanje(int lijeviMotor,int desniMotor)
-    {
-        cmd[0]=(byte) 0xff;
-        cmd[1]=(byte) 0x55;
-        cmd[2]=(byte) 8;
-        cmd[3]=(byte) 0;
-        cmd[4]=(byte) WRITEMODULE;
-        cmd[5]=(byte) type;
+    private void Kretanje(int lijeviMotor, int desniMotor) {
+        cmd[0] = (byte) 0xff;
+        cmd[1] = (byte) 0x55;
+        cmd[2] = (byte) 8;
+        cmd[3] = (byte) 0;
+        cmd[4] = (byte) WRITEMODULE;
+        cmd[5] = (byte) type;
         final ByteBuffer buf = ByteBuffer.allocate(4);
         buf.putShort((short) lijeviMotor);
         buf.putShort((short) desniMotor);
@@ -236,96 +235,100 @@ public class KontroleActivity extends AppCompatActivity {
         cmd[7] = b1;
         cmd[8] = b4;
         cmd[9] = b3;
-        cmd[10]=(byte) '\n';
+        cmd[10] = (byte) '\n';
 
-        try{
+        try {
             bluetoothSocket.getOutputStream().write(cmd);
-        }catch(IOException e){
+        } catch (IOException e) {
 
         }
     }
 
     //metoda za promjenu brzine kretanja
-    private void PromjenaBrzine(int desniMotor, int lijeviMotor){
+    private void PromjenaBrzine(int desniMotor, int lijeviMotor) {
         DesniMotor = desniMotor;
         LijeviMotor = lijeviMotor;
     }
 
-    private String DohvatiTemperaturu(){
-        String test ="";
+    private String DohvatiTemperaturu() {
+        final int MODE_LINE = 0;
+        final int MODE_FORWARD = 1;
+        int commMode = MODE_LINE;
+
+        String test = "";
 
         byte[] cmd = new byte[9];
-        cmd[0]=(byte) 0xff; //0xff
-        cmd[1]=(byte) 0x55; //0x55
-        cmd[2]=(byte) 5; //5
-        cmd[3]=(byte) 0; //3
-        cmd[4]=(byte) 1; //1
-        cmd[5]=(byte) 2; //2
-        cmd[6]=(byte) (13); //6&0xff
-        cmd[7]=(byte) (2); //7&0xff
-        cmd[8]=(byte) '\n'; // '\n'
+        cmd[0] = (byte) 0xff; //0xff
+        cmd[1] = (byte) 0x55; //0x55
+        cmd[2] = (byte) 5; //5
+        cmd[3] = (byte) 7; //7
+        cmd[4] = (byte) 1; //1
+        cmd[5] = (byte) 27; //27
+        cmd[6] = (byte) (13 & 0xff); //13&0xff
+        cmd[7] = (byte) (2 & 0xff); //2&0xff
+        cmd[8] = (byte) '\n'; // '\n'
 
-        try{
+        try {
             bluetoothSocket.getOutputStream().write(cmd);
-        }catch(IOException e){}
+        } catch (IOException e) {
+        }
 
         try {
             int byteCount = bluetoothSocket.getInputStream().available();
 
-            while(byteCount == 0){
+            while (byteCount == 0) {
                 byteCount = bluetoothSocket.getInputStream().available();
             }
 
-            if(byteCount > 0) {
+            if (byteCount > 0) {
                 InputStream inputStream = bluetoothSocket.getInputStream();
                 byte[] podaci = new byte[1024];
                 int count = inputStream.read(podaci);
 
                 if (podaci != null) {
 
-                List<Byte> listaBajtova = new ArrayList<Byte>();
+                    List<Byte> listaBajtova = new ArrayList<>();
 
-                if(count > 0){
-                    for (int i=0;i<count;i++){
-                        Byte b = podaci[i];
-                        listaBajtova.add(b);
-                        Byte[] novaLista = listaBajtova.toArray(new Byte[listaBajtova.size()]);
-                        int[] buffer = new int[listaBajtova.size()];
-                        for (int j=0; j<novaLista.length;j++){
-                            test+=String.format("%02X ", novaLista[j]);
-                            buffer[j] = novaLista[j];
+                    if (count > 0) {
+                        for (int i = 0; i < count; i++) {
+                            Byte b = podaci[i];
+                            listaBajtova.add(b);
+                            Byte[] novaLista = listaBajtova.toArray(new Byte[listaBajtova.size()]);
+                            int[] buffer = new int[listaBajtova.size()];
+                            for (int j = 0; j < novaLista.length; j++) {
+                                test += String.format("%02X ", novaLista[j]);
+                                buffer[j] = novaLista[j];
+                            }
+                            listaBajtova.clear();
                         }
-                        listaBajtova.clear();
                     }
-                } }
                 }
-        }catch (IOException e)
-        {
+            }
+        } catch (IOException e) {
         }
 
-        /*String[] dijelovi = test.split(" ");
-        test= dijelovi[8];
+        String[] dijelovi = test.split(" ");
+        test = dijelovi[6]; //mislimo da je format 197 = 19.7
         int temp = Integer.parseInt(test, 16);
-        test = ""+temp; */
+        float temperatura = (float) temp / 10;
+        test = String.valueOf(temperatura) + " °C";
         return test;
     }
 
     //metoda za odspajanje
-    private void Disconnect(){
-        if (bluetoothSocket!=null) //ako smo spojeni
+    private void Disconnect() {
+        if (bluetoothSocket != null) //ako smo spojeni
         {
-            try
-            {
+            try {
                 bluetoothSocket.close(); //prekini vezu
+            } catch (IOException e) {
             }
-            catch (IOException e)
-            { }
         }
         finish();
     }
 
     //OVO NAS ZANIMA -> VIDLI NA INTERNETU (KINEZ NA git/makeblock-official.cc) test temp
-    Thread ListenInput=new Thread(){
+    Thread ListenInput = new Thread() {
 
     };
 
@@ -335,21 +338,19 @@ public class KontroleActivity extends AppCompatActivity {
         private boolean ConnectSuccess = true; //ako smo došli do ovdje, skoro smo se spojili
 
         @Override
-        protected void onPreExecute()
-        {
-            Toast.makeText(getApplicationContext(),"Povezivanje....",Toast.LENGTH_LONG).show(); //dok traje spajanje
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(), "Povezivanje....", Toast.LENGTH_LONG).show(); //dok traje spajanje
         }
 
         @Override
         protected Void doInBackground(Void... devices) //varijabilan broj parametara, dobro je to tako
         {
-            try
-            {
+            try {
                 if (bluetoothSocket == null || !isBluetoothConnected) //ako nismo spojeni
                 {
-                    ActivityCompat.requestPermissions(KontroleActivity.this,new String[]{Manifest.permission.BLUETOOTH},1);
-                    ActivityCompat.requestPermissions(KontroleActivity.this,new String[]{Manifest.permission.BLUETOOTH_ADMIN},1);
-                    ActivityCompat.requestPermissions(KontroleActivity.this,new String[]{Manifest.permission.BLUETOOTH_PRIVILEGED},1);
+                    ActivityCompat.requestPermissions(KontroleActivity.this, new String[]{Manifest.permission.BLUETOOTH}, 1);
+                    ActivityCompat.requestPermissions(KontroleActivity.this, new String[]{Manifest.permission.BLUETOOTH_ADMIN}, 1);
+                    ActivityCompat.requestPermissions(KontroleActivity.this, new String[]{Manifest.permission.BLUETOOTH_PRIVILEGED}, 1);
 
                     myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();//dohvati naš bluetooth uređaj
 
@@ -360,26 +361,22 @@ public class KontroleActivity extends AppCompatActivity {
                     BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
                     bluetoothSocket.connect();//počinje spajanje
                 }
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 ConnectSuccess = false;//ako se nismo uspjeli spojiti
             }
             return null;
         }
+
         @Override
         protected void onPostExecute(Void result) //nakon pokušaja spajanja, provjeravamo da li je sve u redu
         {
             super.onPostExecute(result);
 
-            if (!ConnectSuccess)
-            {
-                Toast.makeText(getApplicationContext(),"Povezivanje neuspješno",Toast.LENGTH_SHORT).show();
+            if (!ConnectSuccess) {
+                Toast.makeText(getApplicationContext(), "Povezivanje neuspješno", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-            else
-            {
-                Toast.makeText(getApplicationContext(),"Povezivanje uspješno",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Povezivanje uspješno", Toast.LENGTH_SHORT).show();
                 isBluetoothConnected = true;
             }
 
