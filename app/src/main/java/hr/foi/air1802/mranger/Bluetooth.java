e44acfb986e15a1116085f4e9a1dfd13faf0976a
Kristijan Perković <37228334+kristijan-perkovic96@users.noreply.github.com>
2018-11-30 14:48:50 +0100

    Optimizacija koda
    Logička reorganizacija koda.
    
    Co-Authored-By: Kristijan Žebčević <krizebcev@users.noreply.github.com>
    Co-Authored-By: Karlo Pavleka <kpavleka@users.noreply.github.com>

---
+++ app/src/main/java/hr/foi/air1802/mranger/Bluetooth.java
@@ -0,0 +1,89 @@
+package hr.foi.air1802.mranger;
+
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.ActivityCompat;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public final class Bluetooth {
+    public static BluetoothAdapter myBluetoothAdapter;
+    public static ArrayList<BluetoothDevice> myBluetoothDevices;
+    public static ListView listaDiscoveredDevices;
+    public static DeviceListAdapter myDeviceListAdapter;
+    private static String deviceAddress;
+    public static String EXTRA_ADDRESS = "device_address";
+
+    public static void createBluetoothAdapter(){
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
+
+
+    public static void enableDisableBluetooth(Context context){
+
+        if (myBluetoothAdapter == null) { //ne podržava hr.foi.air1802.mranger.Bluetooth
+            Toast.makeText(context,"This device does not support hr.foi.air1802.mranger.Bluetooth connection.",Toast.LENGTH_LONG).show();
+        }
+        if (!myBluetoothAdapter.isEnabled()) { //hr.foi.air1802.mranger.Bluetooth nije uključen
+            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE); //uključi
+            context.startActivity(enableBTIntent);
+        }
+        if (myBluetoothAdapter.isEnabled()) { //hr.foi.air1802.mranger.Bluetooth uključen
+            myBluetoothAdapter.disable(); //isključi
+
+            myBluetoothDevices.clear(); //ukoliko smo već otkrili neke, čistimo
+            listaDiscoveredDevices.setAdapter(null);
+
+            Toast.makeText(context,"hr.foi.air1802.mranger.Bluetooth isključen.",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public  static void discoverBluetoothDevices(Activity activity, Context context, BroadcastReceiver myBroadcastReceiver){
+
+        myBluetoothDevices.clear(); //ukoliko smo već otkrili neke, čistimo
+
+        if (myBluetoothAdapter.isDiscovering()) {
+
+            myBluetoothAdapter.cancelDiscovery(); //poništi otkrivanje
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            //provjera BT dopuštenja u manifestu
+
+            myBluetoothAdapter.startDiscovery();
+        }
+        if (!myBluetoothAdapter.isDiscovering()) {
+
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            //provjera BT dopuštenja u manifestu
+
+            myBluetoothAdapter.startDiscovery();
+        }
+        IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        context.registerReceiver(myBroadcastReceiver, discoverDevicesIntent);
+
+        if (!myBluetoothAdapter.isEnabled()) { //hr.foi.air1802.mranger.Bluetooth nije uključen
+            Toast.makeText(context,"Potrebno je uključiti hr.foi.air1802.mranger.Bluetooth!",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static void startConnection( int position, Context context){
+        myBluetoothAdapter.cancelDiscovery();
+
+        deviceAddress = myBluetoothDevices.get(position).getAddress();
+        myBluetoothDevices.get(position).createBond();
+
+        Intent connection = new Intent(context, KontroleActivity.class);
+        connection.putExtra(EXTRA_ADDRESS, deviceAddress);
+        context.startActivity(connection);
+    }
+}
--- app/src/main/java/hr/foi/air1802/mranger/KontroleActivity.java
+++ app/src/main/java/hr/foi/air1802/mranger/KontroleActivity.java
@@ -1,42 +1,18 @@
 package hr.foi.air1802.mranger;
 
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
-import android.renderscript.ScriptGroup;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
 public class KontroleActivity extends AppCompatActivity {
 
-    String address = null;
-    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-
-    BluetoothAdapter myBluetoothAdapter = null;
-    BluetoothSocket bluetoothSocket = null;
-
-    private boolean isBluetoothConnected = false;
-
     Button gumbDisconnect;
-
     Button gumbForward;
     Button gumbLeft;
     Button gumbRight;
@@ -48,23 +24,13 @@ public class KontroleActivity extends AppCompatActivity {
     //temperatura
     Button gumbTemperatura;
 
-    InputStream inputStream = null;
-    String incomingMessage;
+
     StringBuilder messages;
-
-    //za kretanje podaci
-    byte[] cmd = new byte[13];
-    public static final int WRITEMODULE = 2;
-    public static final int type = 5;
-
-    private int DesniMotor = 180;
-    private int LijeviMotor = 180;
-
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Disconnect();
+        Controls.Disconnect(KontroleActivity.this);
     }
 
     @Override
@@ -73,8 +39,8 @@ public class KontroleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_kontrole);
         Intent newint = getIntent();
         messages = new StringBuilder();
-        address = newint.getStringExtra(MainActivity.EXTRA_ADDRESS);
-        new ConnectBT().execute(); //konekcija
+        Controls.address = newint.getStringExtra(Bluetooth.EXTRA_ADDRESS);
+        new ConnectBT(getApplicationContext(),KontroleActivity.this).execute(); //konekcija
         gumbDisconnect = findViewById(R.id.gumbDisconnect);
 
         //kretanje
@@ -99,23 +65,14 @@ public class KontroleActivity extends AppCompatActivity {
         gumbDisconnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Disconnect();
+                Controls.Disconnect(KontroleActivity.this);
             }
         });
 
         gumbForward.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                    Kretanje(-LijeviMotor, DesniMotor);
-
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-
-                    Kretanje(0, 0);
-                }
-                return true;
+               return Controls.moveForward(event);
             }
 
 
@@ -124,54 +81,28 @@ public class KontroleActivity extends AppCompatActivity {
         gumbLeft.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                    Kretanje(-LijeviMotor, -DesniMotor);
-
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-
-                    Kretanje(0, 0);
-                }
-                return true;
+                return Controls.moveLeft(event);
             }
         });
 
         gumbRight.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                    Kretanje(LijeviMotor, DesniMotor);
-
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-
-                    Kretanje(0, 0);
-                }
-                return true;
+                return Controls.moveRight(event);
             }
         });
 
         gumbBackwards.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                    Kretanje(LijeviMotor, -DesniMotor);
-
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-
-                    Kretanje(0, 0);
-                }
-                return true;
+               return  Controls.moveBackwards(event);
             }
         });
 
         gumbSporo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PromjenaBrzine(120, 120);
+                Controls.changeSpeed(120, 120);
 
                 gumbSporo.setBackgroundColor(Color.parseColor("#4eae68"));
                 gumbNormalno.setBackgroundResource(android.R.drawable.btn_default);
@@ -182,7 +113,7 @@ public class KontroleActivity extends AppCompatActivity {
         gumbNormalno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PromjenaBrzine(180, 180);
+                Controls.changeSpeed(180, 180);
 
                 gumbSporo.setBackgroundResource(android.R.drawable.btn_default);
                 gumbNormalno.setBackgroundColor(Color.parseColor("#fed63c"));
@@ -193,7 +124,7 @@ public class KontroleActivity extends AppCompatActivity {
         gumbBrzo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                PromjenaBrzine(240, 240);
+                Controls.changeSpeed(240, 240);
 
                 gumbSporo.setBackgroundResource(android.R.drawable.btn_default);
                 gumbNormalno.setBackgroundResource(android.R.drawable.btn_default);
@@ -205,165 +136,9 @@ public class KontroleActivity extends AppCompatActivity {
         gumbTemperatura.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gumbTemperatura.setText(DohvatiTemperaturu());
+                gumbTemperatura.setText(Controls.getTemperature());
             }
         });
     }
 
-    //metoda za kretanje
-    private void Kretanje(int lijeviMotor, int desniMotor) {
-        cmd[0] = (byte) 0xff;
-        cmd[1] = (byte) 0x55;
-        cmd[2] = (byte) 8;
-        cmd[3] = (byte) 0;
-        cmd[4] = (byte) WRITEMODULE;
-        cmd[5] = (byte) type;
-        final ByteBuffer buf = ByteBuffer.allocate(4);
-        buf.putShort((short) lijeviMotor);
-        buf.putShort((short) desniMotor);
-        buf.position(0);
-        // Read back bytes
-        final byte b1 = buf.get();
-        final byte b2 = buf.get();
-        final byte b3 = buf.get();
-        final byte b4 = buf.get();
-        cmd[6] = b2;
-        cmd[7] = b1;
-        cmd[8] = b4;
-        cmd[9] = b3;
-        cmd[10] = (byte) '\n';
-
-        try {
-            bluetoothSocket.getOutputStream().write(cmd);
-        } catch (IOException e) {
-        }
-    }
-
-    //metoda za promjenu brzine kretanja
-    private void PromjenaBrzine(int desniMotor, int lijeviMotor) {
-        DesniMotor = desniMotor;
-        LijeviMotor = lijeviMotor;
-    }
-
-    private String DohvatiTemperaturu() {
-        String test = "";
-
-        byte[] cmd = new byte[9];
-        cmd[0] = (byte) 0xff; //0xff
-        cmd[1] = (byte) 0x55; //0x55
-        cmd[2] = (byte) 5; //5
-        cmd[3] = (byte) 7; //7 indeks na kojem se nalazi senzor
-        cmd[4] = (byte) 1; //1
-        cmd[5] = (byte) 27; //27 tip uređaja
-        cmd[6] = (byte) (13 & 0xff); //13&0xff- mora biti port 13 za senzor
-        cmd[7] = (byte) (2 & 0xff); //2&0xff- slot senzora čije uzimamo vrijednosti
-        cmd[8] = (byte) '\n'; // '\n'
-
-        try {
-            bluetoothSocket.getOutputStream().write(cmd);//uključivanje moda mjerenja temperature
-            int byteCount = bluetoothSocket.getInputStream().available();
-            while (byteCount == 0) {
-                byteCount = bluetoothSocket.getInputStream().available();
-            }
-
-            if (byteCount > 0) {
-                InputStream inputStream = bluetoothSocket.getInputStream();//uzimanje podataka od robota
-                byte[] podaci = new byte[1024];
-                int count = inputStream.read(podaci);
-
-                if (podaci != null) {
-
-                    List<Byte> listaBajtova = new ArrayList<>();
-
-                    if (count > 0) {
-                        for (int i = 0; i < count; i++) {
-                            Byte b = podaci[i];
-                            listaBajtova.add(b);
-                            Byte[] novaLista = listaBajtova.toArray(new Byte[listaBajtova.size()]);
-                            int[] buffer = new int[listaBajtova.size()];
-                            for (int j = 0; j < novaLista.length; j++) {
-                                test += String.format("%02X ", novaLista[j]);
-                                buffer[j] = novaLista[j];
-                            }
-                            listaBajtova.clear();
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {        }
-
-        String[] dijelovi = test.split(" ");
-        test = dijelovi[6]; //mislimo da je format 197 = 19.7, dohvaćamo sa 6 pozicije zato što je tu data
-        int temp = Integer.parseInt(test, 16);
-        float temperatura = (float) temp / 10;
-        test = String.valueOf(temperatura) + " °C";
-        return test;
-    }
-
-    //metoda za odspajanje
-    private void Disconnect() {
-        if (bluetoothSocket != null) //ako smo spojeni
-        {
-            try {
-                bluetoothSocket.close(); //prekini vezu
-            } catch (IOException e) {}
-        }
-        finish();
-    }
-
-    //OVO NAS ZANIMA -> VIDLI NA INTERNETU (KINEZ NA git/makeblock-official.cc) test temp
-    Thread ListenInput = new Thread() {
-
-    };
-
-
-    public class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
-    {
-        private boolean ConnectSuccess = true; //ako smo došli do ovdje, skoro smo se spojili
-
-        @Override
-        protected void onPreExecute() {
-            Toast.makeText(getApplicationContext(), "Povezivanje....", Toast.LENGTH_LONG).show(); //dok traje spajanje
-        }
-
-        @Override
-        protected Void doInBackground(Void... devices) //varijabilan broj parametara, dobro je to tako
-        {
-            try {
-                if (bluetoothSocket == null || !isBluetoothConnected) //ako nismo spojeni
-                {
-                    ActivityCompat.requestPermissions(KontroleActivity.this, new String[]{Manifest.permission.BLUETOOTH}, 1);
-                    ActivityCompat.requestPermissions(KontroleActivity.this, new String[]{Manifest.permission.BLUETOOTH_ADMIN}, 1);
-                    ActivityCompat.requestPermissions(KontroleActivity.this, new String[]{Manifest.permission.BLUETOOTH_PRIVILEGED}, 1);
-
-                    myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();//dohvati naš bluetooth uređaj
-
-                    BluetoothDevice bluetoothRobot = myBluetoothAdapter.getRemoteDevice(address);
-
-                    //spaja se na adresu uređaja i provjerava da li je slobodna
-                    bluetoothSocket = bluetoothRobot.createInsecureRfcommSocketToServiceRecord(myUUID);//stvara RFCOMM (SPP) vezu
-                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
-                    bluetoothSocket.connect();//počinje spajanje
-                }
-            } catch (IOException e) {
-                ConnectSuccess = false;//ako se nismo uspjeli spojiti
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) //nakon pokušaja spajanja, provjeravamo da li je sve u redu
-        {
-            super.onPostExecute(result);
-
-            if (!ConnectSuccess) {
-                Toast.makeText(getApplicationContext(), "Povezivanje neuspješno", Toast.LENGTH_SHORT).show();
-                finish();
-            } else {
-                Toast.makeText(getApplicationContext(), "Povezivanje uspješno", Toast.LENGTH_SHORT).show();
-                isBluetoothConnected = true;
-            }
-
-        }
-    }
 }
